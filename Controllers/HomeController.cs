a78adc224bab319ac7541c9366172cf48987f219
mohsen <71mohsenpsh@gmail.com>
2024-07-02 10:16:16 +0330

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebClient.Models;
+
+namespace WebClient.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private CustomerRepository _customer;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _customer = new CustomerRepository();
+        }
+
+        public IActionResult Index()
+        {
+            string token = User.FindFirst("AccessToken").Value;
+            return View(_customer.GetAllCustomer(token));
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            _customer.AddCustomer(customer);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var customer = _customer.GetCustomerById(id);
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            _customer.UpdateCustomer(customer);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            _customer.DeleteCustomer(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
